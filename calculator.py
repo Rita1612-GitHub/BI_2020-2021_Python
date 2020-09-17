c653b40baacf57cf94f5a3ac5c28002e19f0dc78
Rita <komarovamy96@yandex.ru>
2020-09-18 00:18:39 +0300

    Calculator update

--- calculator.py
+++ calculator.py
@@ -1,8 +1,8 @@
-#Enter numbers and operator
+# Enter numbers and operator
 number1 = float(input('Enter the first number: '))
 operator = input('Enter the operator: ')
 number2 = float(input('Enter the second number: '))
-#Checking
+# Checking
 if operator == "+":
     print(number1 + number2)
 elif operator == "-":
@@ -10,7 +10,7 @@ elif operator == "-":
 elif operator == "*":
     print(number1 * number2)
 elif operator == "/":
-    if number2 != 0: #Check devision by zero
+    if number2 != 0: # Check devision by zero
         print(number1 / number2)
     else:
         print("Division by zero!")
@@ -22,4 +22,3 @@ elif operator == "//":
     print(number1 // number2)
 else:
     print("Wrong operator")
-
